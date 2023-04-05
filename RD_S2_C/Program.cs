20a1835f316d021bf39c0011716c1c5e2d1ffdb1
DimasVhigarr <dimasvhigar2002@gmail.com>
2023-04-05 16:41:34 +0700

    create koneksi ke database

--- RD_S2_C/Program.cs
+++ RD_S2_C/Program.cs
@@ -11,7 +11,30 @@ namespace RD_S2_C
     class Program
     {
         
-        
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Koneksi Ke Database\n");
+                    Console.WriteLine("Masukkan User ID: ");
+                    string user = Console.ReadLine();
+                    Console.WriteLine("Masukkan Password: ");
+                    string pass = Console.ReadLine();
+                    Console.WriteLine("Masukkan Database tujuan: ");
+                    string db = Console.ReadLine();
+                    Console.Write("\nKetik K untuk terhubung ke Database: ");
+                    char chr = Convert.ToChar(Console.ReadLine());
+                    switch (chr)
+                    {
+                        
+                    }
+                }
+                
+            }
+        }
 
         
 
